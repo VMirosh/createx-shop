b2c2ac3eb8e031a6f8cc26d8a522d347ebb6c549
Vladyslav Miroshnychenko <miroshnychenko.vlad@gmail.com>
2023-04-17 12:32:57 +0200

    add_major_components

---
+++ src/Components/App.js
@@ -0,0 +1,17 @@
+import "./reset.css";
+import "./App.css";
+import Header from "./Header/Header";
+import Main from "./Main/Main";
+import Footer from "./Footer/Footer";
+function App() {
+  return (
+    <div className="wrapper">
+        <Header/>
+        <Main/>
+        <Footer/>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,9 @@
+import "./Main.scss";
+const Main = () =>{
+    return(
+        <main className="main">
+            Main
+        </main>
+    );
+}
+export default Main;
